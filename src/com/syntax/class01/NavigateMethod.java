12de979767f29225dd97210c73ecd986078beb9b
muhammadriaz0505 <muhammadriaz11446@gmail.com>
2022-06-30 14:56:17 -0400

    Selenium class01

---
+++ src/com/syntax/class01/NavigateMethod.java
@@ -0,0 +1,19 @@
+package com.syntax.class01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigateMethod {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.facebook.com");
+        driver.navigate().to("http://google.com");
+        driver.navigate().back();
+        driver.navigate().forward();
+        Thread.sleep(5000);
+        driver.navigate().refresh();
+        //driver.close();  will close the current tab
+        driver.quit();// will quit the whole browser
+    }
+}
---
+++ src/com/syntax/class01/Task.java
@@ -0,0 +1,16 @@
+package com.syntax.class01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.amazon.com");
+        String url=driver.getCurrentUrl();
+        String title=driver.getTitle();
+        System.out.println(title+" "+url);
+        driver.quit();
+    }
+}
