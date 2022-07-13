17885216f8b3835ad4af194cd40e49fda6d5031f
muhammadriaz0505 <muhammadriaz11446@gmail.com>
2022-07-13 19:04:58 -0400

    Selenium Class 05

---
+++ src/com/syntax/class05/HandlingDropDown.java
@@ -0,0 +1,36 @@
+package com.syntax.class05;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class HandlingDropDown {
+
+        public static String url="https://syntaxprojects.com/basic-select-dropdown-demo.php";
+        public static void main(String[] args) throws InterruptedException {
+            System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+            WebDriver driver=new ChromeDriver();
+            driver.get(url);
+            WebElement daysDD=driver.findElement(By.id("select-demo"));
+            Select select=new Select(daysDD);
+            select.selectByIndex(3);
+            Thread.sleep(2000);
+            select.selectByVisibleText("Thursday");
+            Thread.sleep(1000);
+            select.selectByValue("Friday");
+List<WebElement> allOptions=select.getOptions();
+int size=allOptions.size();
+            System.out.println(size);
+            for(int i=1; i<size; i++){
+               String options= allOptions.get(i).getText();
+                System.out.println(options);
+                select.selectByIndex(i);
+                Thread.sleep(1000);
+            }
+
+    }
+}
